daf1a2b1289a9b80b486affa0dc5c63e467cf42f
Andrew <andrewxieax1@gmail.com>
2021-11-03 17:57:20 -0700

    contouring on edge detection

--- experiments/license_ROI_detection/plate_contour.py
+++ experiments/license_ROI_detection/plate_contour.py
@@ -2,39 +2,43 @@ import cv2
 import rospy
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
+import imutils
 
 
 def proccess_image(data):
     cv_image = bridge.imgmsg_to_cv2(data, desired_encoding='bgr8')
+    cv_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     rect_contours(cv_image)
 
 def rect_contours(img):
-    ''':param: img image'''
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    edged = cv2.Canny(gray, 30, 200)
-    cnts= cv2.findContours(edged, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[0]
-    print(cnts)
-    cv2.drawContours(img, cnts, -1, (0,255,0), 3)
-    cv2.imshow('a', img)
-    cv2.waitKey(0)
-    return
+    ''':param: gray image'''
     
-    cnts=sorted(cnts, key = cv2.contourArea, reverse = True)[:10]
-
-    # loop over our contours
-    for c in cnts:
+    
+    edged = cv2.Canny(gray, 30, 200) 
+    cv2.imshow('a', edged)
+    cv2.waitKey(0)
+    contours = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours = imutils.grab_contours(contours)
+    contours = sorted(contours, key = cv2.contourArea, reverse = True)[:30]
+    screenCnt = None
+    
+    screenCnts = []
+    for c in contours:
+        
         peri = cv2.arcLength(c, True)
         approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-        if len(approx) == 4:
-            cv2.drawContours(img, [approx], -1, (0,255,0), 3)
     
+        if len(approx) == 4:
+            screenCnts.append(approx)
+
+    cv2.drawContours(img, screenCnts, -1, (0, 0, 255), 3)
+
     cv2.imshow('a', img)
     cv2.waitKey(0)
-
     
 
 if __name__ == '__main__':
-    img = cv2.imread('test_license.png')
+    img = cv2.imread('./../test_images/closeup.png')
     
     rect_contours(img)
     quit()
